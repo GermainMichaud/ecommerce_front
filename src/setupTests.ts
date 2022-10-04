0d1d518ec776d12e9ad906e9780df68eac527815
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-04 09:12:56 +0200

    feat: setup msw

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,23 @@
+import 'whatwg-fetch';
+
 import matchers from '@testing-library/jest-dom/matchers';
+import { setupServer } from 'msw/node';
 import { expect } from 'vitest';
 
+import { handlers } from './mocks/handlers';
+
 expect.extend(matchers);
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
