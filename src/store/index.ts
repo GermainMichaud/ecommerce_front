b233bbe8f48aa717a34ebdbcf1fc8e7f5ccd4ac9
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:55:40 +0200

    feat: create the redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, PreloadedState } from '@reduxjs/toolkit';
 
-export const createStore = () =>
+import cartReducer from './cart/cartSlice';
+import { orderApi } from './order/orderHook';
+import { productsApi } from './products/productsHook';
+
+export const rootReducer = combineReducers({
+  cart: cartReducer,
+  [productsApi.reducerPath]: productsApi.reducer,
+  [orderApi.reducerPath]: orderApi.reducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
   configureStore({
-    reducer: {},
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(productsApi.middleware),
+    preloadedState,
   });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
 
-export const store = createStore();
+export const store = setupStore();
