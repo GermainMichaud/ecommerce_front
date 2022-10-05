edf9d8cbbb2a7196ca2f9635ebc92b7e6d96259a
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:58:35 +0200

    feat: add redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,27 @@ import './index.css';
 import { Router } from '@tanstack/react-location';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import { location, routes } from './routes';
+import { store } from './store';
+
+// if (import.meta.env.MODE === 'development') {
+//   const { worker } = await import('./mocks/browser');
+//   await worker.start();
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router
-      location={location}
-      routes={routes}
-      defaultPendingElement={<div>Loading...</div>}
-    >
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router
+        location={location}
+        routes={routes}
+        defaultPendingElement={<div>Loading...</div>}
+      >
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
