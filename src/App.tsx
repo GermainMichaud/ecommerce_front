d25bf362bc4d32518f4e73099adb26ee97af56b8
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:59:01 +0200

    feat: add base routes + navbar + footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import './App.css';
 
 import { Outlet } from '@tanstack/react-location';
 
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { Wrapper } from './components/ui/common/Wrapper';
+
 const App = () => (
   <>
-    <h1>Ecommerce</h1>
-    <Outlet />
+    <Navbar />
+    <Wrapper maxWidth="800px">
+      <Outlet />
+    </Wrapper>
+    <Footer />
   </>
 );
 
